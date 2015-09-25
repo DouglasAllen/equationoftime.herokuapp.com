04ec8c7b2d7195c676da4f193c1360dac9a1249b
Douglas G. Allen <kb9agt@gmail.com>
2015-09-25 14:57:31 -0500

    Update database.rb

--- config/database.rb
+++ config/database.rb
@@ -1,40 +1,5 @@
-configure :production do
-
-  # Heroku controls what database we connect to by setting the DATABASE_URL 
-  # environment variable.
-  # We need to respect that if we want our Sinatra apps to run on Heroku without modification
-  db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{APP_NAME}_#{Sinatra::Application.environment}")
-
-  DB_NAME = db.path[1..-1]
-
-  # Note:
-  #   Sinatra::Application.environment is set to the value of ENV['RACK_ENV']
-  #   if ENV['RACK_ENV'] is set.  If ENV['RACK_ENV'] is not set, it defaults
-  #   to :development
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :port     => db.port,
-    :username => db.user,
-    :password => db.password,
-    :database => DB_NAME,
-    :encoding => 'utf8'
-  )
-end
-
-configure :development do
-
-  # Log queries to STDOUT in development
-  if Sinatra::Application.development?
-    ActiveRecord::Base.logger = Logger.new(STDOUT)
-    ActiveRecord::Base.establish_connection(
-      adapter:  'sqlite3',
-      database: 'db/development.sqlite3',
-      show_exceptions: true,
-      encoding: 'utf8'
-    )
-  end
+if ENV.fetch("RACK_ENV") == "development"
+  p "you're in #{__FILE__}"
 end
 
 configure do   
@@ -53,10 +18,59 @@ configure do
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
 
+  APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
     filename = File.basename(model_file).gsub('.rb', '')
     autoload ActiveSupport::Inflector.camelize(filename), model_file
   end
 end
 
+#include ActiveRecord
+#include ActiveRecord::ConnectionAdapters
 
+database_config = YAML.load_file(File.join( [APP_ROOT, 'config', 'database.yml'] ))
+#spec = ConnectionSpecification::Resolver.new(database_config).spec(:development)
+
+configure :production do
+
+  ActiveRecord::Base.establish_connection(database_config.fetch('default'))
+  # Heroku controls what database we connect to by setting the DATABASE_URL 
+  # environment variable.
+  # We need to respect that if we want our Sinatra apps to run on Heroku without modification
+  #url = ENV['DATABASE_URL']
+  #db  = URI.parse(url)
+  # db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{APP_NAME}_#{Sinatra::Application.environment}")
+
+  #DB_NAME = db.path[1..-1]
+
+  # Note:
+  #   Sinatra::Application.environment is set to the value of ENV['RACK_ENV']
+  #   if ENV['RACK_ENV'] is set.  If ENV['RACK_ENV'] is not set, it defaults
+  #   to :development
+  
+  #ActiveRecord::Base.establish_connection(
+  #  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  #  :host     => db.host,
+  #  :port     => db.port,
+  #  :username => db.user,
+  #  :password => db.password,
+  #  :database => DB_NAME,
+  #  :encoding => 'utf8'
+  #)  
+end
+
+configure :development do
+
+  # Log queries to STDOUT in development
+#  if Sinatra::Application.development?
+#    DB_NAME = 'db/development.sqlite3',
+#    ActiveRecord::Base.logger = Logger.new(STDOUT)    
+#    ActiveRecord::Base.establish_connection(
+#      adapter:  'sqlite3',
+#      database: 'db/development.sqlite3',      
+#      show_exceptions: true,
+#      encoding: 'utf8'
+#    )
+#    ActiveRecord::Base.establish_connection(database_config.fetch('development'))
+#  end
+end
