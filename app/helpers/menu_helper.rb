7ad27a5ea471a0d35497a08f6ea1e952aae1ea71
Douglas G. Allen <kb9agt@gmail.com>
2015-09-25 16:32:38 -0500

    Create menu_helper.rb

---
+++ app/helpers/menu_helper.rb
@@ -0,0 +1,22 @@
+
+helpers do
+
+    if ENV.fetch("RACK_ENV") == "development"
+      p "you're in #{__FILE__}"
+    end
+
+    @mc = MenuController.new
+
+    HOME     = @mc.home
+    TUTORIAL = @mc.tutorial
+    GRAPH    = @mc.graph
+    DATA     = @mc.data
+    EOT      = @mc.eot
+    MD       = @mc.md
+    RDOC     = @mc.rdoc
+    GM       = @mc.gm
+    LINKS    = @mc.links
+    EXAMPLES = @mc.examples
+  
+
+end
